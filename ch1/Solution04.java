b46f77e57508fd1a0e89dda8946d76094450c1b4
liqingcmu <liqingfd@gmail.com>
2015-09-12 20:31:07 -0400

    Create Solution04.java
    The key point of solving the problem is that: At most one character of the string can have an odd count.[1]
    [1] McDowell G L. Cracking the coding interview[M], 2015, p195

---
+++ ch1/Solution04.java
@@ -0,0 +1,28 @@
+public static boolean isPalindromePermutation(String str){
+		int oddCount = 0;
+		char[] arrayOfStr = str.toCharArray();
+		int[] table = new int[Character.getNumericValue('z')-Character.getNumericValue('a')+1];
+		for (char c :arrayOfStr){
+			int x = getElemNumber(c);
+			if(x != -1){
+				table[x]++;
+				if (table[x] % 2 == 0)
+					oddCount--;
+				else
+					oddCount++;
+			}
+		}
+		return oddCount<=1;
+	}
+	public static int getElemNumber(char c){
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		int A = Character.getNumericValue('A');
+		int Z = Character.getNumericValue('Z');
+		int val = Character.getNumericValue(c);
+		if (val >= a && val <= z)
+			return val - 'a';
+		else if (val >= A && val <= Z)
+			return val - 'A';
+		return -1;
+	}
