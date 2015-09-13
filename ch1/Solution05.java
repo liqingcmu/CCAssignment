a964c9dfac7046774b934257465421ede3180ccc
liqingcmu <liqingfd@gmail.com>
2015-09-12 20:32:23 -0400

    Create Solution05.java
    The removal operation can be considered to be the inverse of the insertion.[1]
    [1] McDowell G L. Cracking the coding interview[M], 2015, p199

---
+++ ch1/Solution05.java
@@ -0,0 +1,42 @@
+public static boolean oneAway(String str1, String str2){
+		int length1 = str1.length();
+		int length2 = str2.length();
+		if (length1 == length2) //replacement
+			return replacement(str1, str2);
+		else if (length1 == length2+1) //remove
+			return remove(str1, str2);
+		else if (length2 == length1+1) //insert
+			return remove(str2, str1);
+		return false;
+	}
+	public static boolean replacement(String str1, String str2){
+		int length = str1.length();
+		boolean ifHasOne = false;
+		for (int i = 0; i < length; i++){
+			if( str1.charAt(i) != str2.charAt(i)){
+				if (ifHasOne == true)
+					return false;
+				ifHasOne = true;
+			}
+		}
+		return true;
+	}
+	public static boolean remove(String str1, String str2){
+		int length1 = str1.length();
+		int length2 = str2.length();
+		int ptr1, ptr2;
+		boolean ifHasOne = false;
+		for (ptr1 = 0, ptr2 = 0; ptr1 < length1 && ptr2 < length2;){
+			if (str1.charAt(ptr1) != str2.charAt(ptr2)){
+				if (ifHasOne == true)
+					return false;
+				ifHasOne = true;
+				ptr1++;
+			}
+			else{
+				ptr1++;
+				ptr2++;
+			}
+		}
+		return true;
+	}
